58ab3d4dc610c74dc1ef904f019db6e150cd1744
Ashley Zemina <amzemina@gmail.com>
2023-07-18 19:51:20 -0400

    deleted some comments

--- assets/script.js
+++ assets/script.js
@@ -38,20 +38,6 @@ $(document).ready(function () {
 
 
     //Edamam Api recipies
-    // function getRecipieEdamam(recipie) {
-
-    //     $.ajax({
-    //         url: 'https://api.edamam.com/api/recipes/v2?type=public&beta=false&app_id=a6d7f68a&app_key=8b4d17c343033fa42a2337d2263c8846&ingr=8&diet=balanced&diet=high-fiber&diet=high-protein&diet=low-carb&diet=low-fat&diet=low-sodium&health=wheat-free&cuisineType=American&cuisineType=Asian&cuisineType=British&cuisineType=Caribbean&cuisineType=Central%20Europe&cuisineType=Chinese&cuisineType=Eastern%20Europe&cuisineType=French&cuisineType=Indian&cuisineType=Italian&cuisineType=Japanese&cuisineType=Kosher&cuisineType=Mediterranean&cuisineType=Mexican&cuisineType=Middle%20Eastern&cuisineType=Nordic&cuisineType=South%20American&cuisineType=South%20East%20Asian&mealType=Breakfast&mealType=Dinner&mealType=Lunch&mealType=Snack&mealType=Teatime&dishType=Biscuits%20and%20cookies&dishType=Bread&dishType=Cereals&dishType=Condiments%20and%20sauces&dishType=Desserts&dishType=Drinks&dishType=Main%20course&dishType=Pancake&dishType=Preps&dishType=Preserve&dishType=Salad&dishType=Sandwiches&dishType=Side%20dish&dishType=Soup&dishType=Starter&dishType=Sweets&imageSize=REGULAR&random=true',
-    //         method: 'GET',
-    //         dataType: 'JSON',
-    //         success: function (response) {
-    //             console.log(response)
-    //             var stringResponse = JSON.stringify(response);
-
-    //             localStorage.setItem('apiData', stringResponse);
-    //         }
-    //     })
-    // }
     async function  getRecipieEdamam() {
         const response = await fetch("https://api.edamam.com/api/recipes/v2?type=public&beta=false&app_id=a6d7f68a&app_key=8b4d17c343033fa42a2337d2263c8846&ingr=8&health=wheat-free&cuisineType=American&cuisineType=Asian&cuisineType=British&cuisineType=Caribbean&cuisineType=Central%20Europe&cuisineType=Chinese&cuisineType=Eastern%20Europe&cuisineType=French&cuisineType=Indian&cuisineType=Italian&cuisineType=Japanese&cuisineType=Kosher&cuisineType=Mediterranean&cuisineType=Mexican&cuisineType=Middle%20Eastern&cuisineType=Nordic&cuisineType=South%20American&cuisineType=South%20East%20Asian&mealType=Breakfast&mealType=Dinner&mealType=Lunch&mealType=Snack&mealType=Teatime&random=true");
         const recipies = await response.json();
@@ -75,7 +61,7 @@ $(document).ready(function () {
     function displayMealResults() {
         var recipieResult = $("#recipie-result")
         recipieResult.empty()
-        
+
     }
     displayEdamamResults()
     displayMealResults()
