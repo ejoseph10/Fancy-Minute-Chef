493fce5f238c7d6cebff0c07365ddb8f533fef38
Ashley Zemina <amzemina@gmail.com>
2023-07-19 19:50:35 -0400

    connected submit/search buttons to the apis

--- assets/script.js
+++ assets/script.js
@@ -4,71 +4,51 @@
 
 $(document).ready(function () {
 
-    $("#search-category-btn").on("click", function(event) {
+    $("#search-category-btn").on("click", function (event) {
         event.preventDefault()
 
-        var cuisineDropdownElValue = $("#cuisine-drop-down").val();
-        console.log(cuisineDropdownElValue)
-        var mealDropdownElValue = $("#meal-drop-down").val();
-        console.log(mealDropdownElValue)
-        
-        var apiUrl = 'https://api.edamam.com/api/recipes/v2?type=public&beta=false&app_id=a6d7f68a&app_key=8b4d17c343033fa42a2337d2263c8846'
-        
-        if (cuisineDropdownElValue !== "all") {
-          apiUrl = apiUrl + `&cuisineType=${cuisineDropdownElValue}`;
-        }
-        if (mealDropdownElValue !== "all") {
-          apiUrl = apiUrl + `&mealType=${mealDropdownElValue}`;
-        }
-       
-        getRecipieEdamam(apiUrl)
-      });
+        var edamamSearch = $("#search-input").val();
+        console.log(edamamSearch)
 
-      async function getRecipieEdamam(url){
-        console.log(url)
-        const response = await fetch(url)
-        const recipies = await response.json();
-        console.log(recipies);
+        getRecipeEdamam(edamamSearch)
+    });
+
+    async function getRecipeEdamam(searchQuery) {
+        const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&beta=false&app_id=a6d7f68a&app_key=8b4d17c343033fa42a2337d2263c8846&random=true&q=${searchQuery}`)
+        const recipes = await response.json();
+        console.log(recipes);
+        // TODO: add to localstorage. Decide a standard format to store in that works for both APIs
+    }
+    
+    //Meal Api recipies
+    async function getRecipeMeal() {
+        const response = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
+        const recipes = await response.json();
+        console.log(recipes);
+        // TODO: add to localstorage. Decide a standard format to store in that works for both APIs
     }
 
     //Randomn button click event
-    $("#search-btn").on("click", function (event) {
+    $("#random-meal").on("click", function (event) {
         event.preventDefault()
         
-        var submitBox = $("#search-input")
-        
-        getRecipieMeal(submitBox.val())
+        getRecipeMeal()
     });
 
-    //random meal function
+    // TODO: make a display function 
 
-    // function getRandomMeal(){
-    //     fetch('URL', {
-    //        headers: {
-    //             'Accept': 'appliction/json'
-    //         }
-    //     }).then(data=> data.json())
-    // change randomText to corresponding HTML class & random Meal to corresponding html class
-    //       .then(obj => randomText.innerHTML=obj.random-meal)
-    // }
 
-// main
+
     //displays edamam results
     function displayEdamamResults() {
-        var recipieResult = $("#recipie-result")
-            recipieResult.empty()
+        var recipeResult = $("#recipe-result")
+            recipeResult.empty()
     }
-    //Meal Api recipies
-        async function  getRecipieMeal() {
-            const response = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
-            const recipies = await response.json();
-            console.log(recipies);
-          }
-        
+   
     //displays Meal results
     function displayMealResults() {
-        var recipieResult = $("#recipie-result")
-            recipieResult.empty()
+        var recipeResult = $("#recipe-result")
+            recipeResult.empty()
     }
     displayEdamamResults()
     displayMealResults()
