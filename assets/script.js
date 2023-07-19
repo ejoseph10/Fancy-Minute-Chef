3aad12dfb922414aa0fad4a7188c4c01df193758
Ashley Zemina <amzemina@gmail.com>
2023-07-19 18:12:35 -0400

    added search button function for edamam api

--- assets/script.js
+++ assets/script.js
@@ -4,14 +4,32 @@
 
 $(document).ready(function () {
 
-    //Search Box click event
-    $("#search-btn").on("click", function (event) {
+    $("#search-category-btn").on("click", function(event) {
         event.preventDefault()
 
-        var submitBox = $("#search-input")
+        var cuisineDropdownElValue = $("#cuisine-drop-down").val();
+        console.log(cuisineDropdownElValue)
+        var mealDropdownElValue = $("#meal-drop-down").val();
+        console.log(mealDropdownElValue)
+        
+        var apiUrl = 'https://api.edamam.com/api/recipes/v2?type=public&beta=false&app_id=a6d7f68a&app_key=8b4d17c343033fa42a2337d2263c8846'
+        
+        if (cuisineDropdownElValue !== "all") {
+          apiUrl = apiUrl + `&cuisineType=${cuisineDropdownElValue}`;
+        }
+        if (mealDropdownElValue !== "all") {
+          apiUrl = apiUrl + `&mealType=${mealDropdownElValue}`;
+        }
+       
+        getRecipieEdamam(apiUrl)
+      });
 
-        getRecipieEdamam(submitBox.val())
-    });
+      async function getRecipieEdamam(url){
+        console.log(url)
+        const response = await fetch(url)
+        const recipies = await response.json();
+        console.log(recipies);
+    }
 
     //Randomn button click event
     $("#search-btn").on("click", function (event) {
@@ -35,20 +53,6 @@ $(document).ready(function () {
     // }
 
 // main
-    //Edamam Api recipies
-    function getRecipieEdamam() {
-
-        $.ajax({
-            url: 'https://api.edamam.com/api/recipes/v2?type=public&beta=false&app_id=a6d7f68a&app_key=8b4d17c343033fa42a2337d2263c8846&ingr=8&health=wheat-free&cuisineType=American&cuisineType=Asian&cuisineType=British&cuisineType=Caribbean&cuisineType=Central%20Europe&cuisineType=Chinese&cuisineType=Eastern%20Europe&cuisineType=French&cuisineType=Indian&cuisineType=Italian&cuisineType=Japanese&cuisineType=Kosher&cuisineType=Mediterranean&cuisineType=Mexican&cuisineType=Middle%20Eastern&cuisineType=Nordic&cuisineType=South%20American&cuisineType=South%20East%20Asian&mealType=Breakfast&mealType=Dinner&mealType=Lunch&mealType=Snack&mealType=Teatime&random=true',
-            method: 'GET',
-            dataType: 'JSON',
-            success: function (response) {
-                var stringResponse = JSON.stringify(response);
-
-                localStorage.setItem('apiData', stringResponse);
-            }
-        })
-    }
     //displays edamam results
     function displayEdamamResults() {
         var recipieResult = $("#recipie-result")
