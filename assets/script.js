1433eb8cb1f5043b3c006f2627a892df5ce9598f
Ashley Zemina <amzemina@gmail.com>
2023-07-19 21:06:57 -0400

    added local storage

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,7 @@
 
 $(document).ready(function () {
 
+    //Edamam button click event
     $("#search-category-btn").on("click", function (event) {
         event.preventDefault()
 
@@ -13,43 +14,48 @@ $(document).ready(function () {
         getRecipeEdamam(edamamSearch)
     });
 
+    //Edamam Api recipe
     async function getRecipeEdamam(searchQuery) {
         const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&beta=false&app_id=a6d7f68a&app_key=8b4d17c343033fa42a2337d2263c8846&random=true&q=${searchQuery}`)
         const recipes = await response.json();
         console.log(recipes);
+        if (recipes) {
+            var currentSearches = JSON.parse(localStorage.getItem("recentSearches")) || [];
+            if (
+                !currentSearches.includes(searchQuery)
+            )
+            localStorage.setItem('recentSearches', JSON.stringify([...currentSearches, searchQuery]));
         // TODO: add to localstorage. Decide a standard format to store in that works for both APIs
-    }
-    
-    //Meal Api recipies
-    async function getRecipeMeal() {
-        const response = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
-        const recipes = await response.json();
-        console.log(recipes);
-        // TODO: add to localstorage. Decide a standard format to store in that works for both APIs
+        }
     }
 
-    //Randomn button click event
+    //MealDB button click event
     $("#random-meal").on("click", function (event) {
         event.preventDefault()
-        
+
         getRecipeMeal()
     });
 
+    //Meal Api recipes
+    async function getRecipeMeal() {
+        const response = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
+        const recipeList = await response.json();
+        console.log(recipeList);
+        if (recipeList) {
+            localStorage.setItem('apiData', JSON.stringify(recipeList));
+        }
+        // TODO: add to localstorage. Decide a standard format to store in that works for both APIs
+    }
+
+    
     // TODO: make a display function 
+    function displayResults() {
+    var edResult = $("#recipe-result")
+    var dbResult = $("#recipe-result")
+        edResult.empty()
+        dbResult.empty()
+    
+    } 
 
-
-
-    //displays edamam results
-    function displayEdamamResults() {
-        var recipeResult = $("#recipe-result")
-            recipeResult.empty()
-    }
-   
-    //displays Meal results
-    function displayMealResults() {
-        var recipeResult = $("#recipe-result")
-            recipeResult.empty()
-    }
-    displayEdamamResults()
-    displayMealResults()
+    displayResults()
 })
