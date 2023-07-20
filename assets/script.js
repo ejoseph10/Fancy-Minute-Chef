ba96d0126e28854f148c7379640d476a70e95e4d
Ashley Zemina <amzemina@gmail.com>
2023-07-19 22:53:23 -0400

    displaying mealdb data in html

--- assets/script.js
+++ assets/script.js
@@ -15,9 +15,9 @@ $(document).ready(function () {
         
     });
 
-    //Edamam Api recipe
+    //Edamam fetch Api recipe
     async function getRecipeEdamam(searchQuery) {
-        const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&beta=false&app_id=a6d7f68a&app_key=8b4d17c343033fa42a2337d2263c8846&random=true&q=${searchQuery}`)
+        const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&beta=false&app_id=a6d7f68a&app_key=8b4d17c343033fa42a2337d2263c8846&random=true&q=${searchQuery}&imageSize=SMALL`)
         const recipes = await response.json();
         console.log(recipes);
         if (recipes) {
@@ -31,6 +31,7 @@ $(document).ready(function () {
         }
     }
 
+    //Displays edamam search history under searchbox
     function displayRecentSearches() {
         var recentSearchEl = $("#recent-search-container");
         var currentSearches =  getRecentSearchesFromLS(); 
@@ -44,10 +45,12 @@ $(document).ready(function () {
         })
     }
 
+    //Gets edamam data from localstorage 
     function getRecentSearchesFromLS() {
         return JSON.parse(localStorage.getItem("recentSearches")) || [];
     }
 
+    //Displays edamam search results
     function displaySearchResultsEdamam(results) {
         console.log(results)
         var resultContainerEl = $("#recipe-result-container");
@@ -57,14 +60,11 @@ $(document).ready(function () {
             var recipeEl = $(`
                 <div> 
                     <h3>${hit.recipe.label}</h3>
-                    <a target="_blank" href="${hit.recipe.url}">
-                    <img src="${hit.recipe.images.THUMBNAIL.url}" alt="recipe picture"></a> 
                     <div>${hit.recipe.dietLabels}</div>
                     <div>${hit.recipe.mealType} - ${hit.recipe.dishType}</div>
+                    <a target="_blank" href="${hit.recipe.url}">
+                    <img src="${hit.recipe.images.SMALL.url}" alt="recipe picture"></a> 
                 </div>`)
-            recipeEl.on("click", function(){
-                getRecipeEdamam(search)
-            })
             resultContainerEl.append(recipeEl)
         })
     }
@@ -76,15 +76,34 @@ $(document).ready(function () {
         getRecipeMeal()
     });
 
-    //Meal Api recipes
+    //Meal fetch Api recipes
     async function getRecipeMeal() {
         const response = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
         const recipeList = await response.json();
         console.log(recipeList);
         if (recipeList) {
             localStorage.setItem('apiData', JSON.stringify(recipeList));
+            displaySearchResultsMealDB(recipeList)
         }
         // TODO: add to localstorage. Decide a standard format to store in that works for both APIs
+    }
+
+    //Displays MealDB recipe
+    function displaySearchResultsMealDB(results) {
+        console.log(results)
+        var resultContainerEl = $("#recipe-result-container");
+        resultContainerEl.empty()
+
+        results.meals.forEach(meal => {
+            var recipeEl = $(`
+                <div> 
+                    <h3>${meal.strMeal}</h3>
+                    <div>${meal.strCategory}</div>
+                    <a target="_blank" href="${meal.strSource}">
+                    <img width="200px" src="${meal.strMealThumb}" alt="recipe picture"></a> 
+                </div>`)
+            resultContainerEl.append(recipeEl)
+        })
     }
 
     displayRecentSearches()  
